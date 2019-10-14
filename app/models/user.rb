f90eef4290563f21a55ebe7afbe23f2a781c5013
Lluis Toyos <ltoyos@gmail.com>
2019-10-14 20:31:32 +0200

    6.3.3 Minimum password standards

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
             length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-
+  validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
