073e08f597a0f7499fc9872a1a58324f35c4713c
Lluis Toyos <ltoyos@gmail.com>
2019-10-14 17:14:08 +0200

    6.2.2 Validating presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  #or validates(:name, presence: true)
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,4 +7,12 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+  test "name should be present" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
 end
