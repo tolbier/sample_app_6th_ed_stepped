1b8c375ac0dd81e7f130969327377a478d4b1aed
Lluis Toyos <ltoyos@gmail.com>
2019-10-15 12:55:51 +0200

    7.3.1 A working form (Not yet finished)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
 end
