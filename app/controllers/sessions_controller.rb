88146ab8acd79df4da8106443e3820e401cbfb3a
Lluis Toyos <ltoyos@gmail.com>
2019-10-15 18:14:35 +0200

    8.1.3 Finding and authenticating a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user&.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
+      render 'new'
+    end
   end
+
+  def destroy; end
 end
