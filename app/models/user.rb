6f8486d1b5709d8793192a14bd6d815a2911cd46
Lluis Toyos <ltoyos@gmail.com>
2019-10-16 12:15:24 +0200

    9.1.2 Login with remembering

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,12 @@ class User < ApplicationRecord
 
   # Remembers a user in the database for use in persistent sessions.
   def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
+    @remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(@remember_token))
   end
 
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
