585360bf307a414b8e7c57d403c447b3857547d5
Lluis Toyos <ltoyos@gmail.com>
2019-10-17 13:21:08 +0200

    11.2.2 Email previews

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,23 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000' #replace with your own url
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['GMAIL_USER'],
+    :password             => ENV['GMAIL_PASS'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
