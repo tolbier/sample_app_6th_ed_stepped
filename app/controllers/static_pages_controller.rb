e42bde967c443f6505b31388d579a4286ddbc34f
Lluis Toyos <ltoyos@gmail.com>
2019-10-18 17:16:10 +0200

    13.3.3 A proto-feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
+
   def help; end
 
   def about; end
